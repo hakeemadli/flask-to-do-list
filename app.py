fc32914639a043b043d10a6dfe68d45f9e4f647e
hakeemadli <muhdhakeemadli@gmail.com>
2023-10-10 17:31:43 +0800

    creating separate files for db schema

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask,render_template,request,redirect,url_for
-from flask_sqlalchemy import SQLAlchemy
+from models import db,Model
 
 
 app = Flask(__name__, template_folder="templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///model.db'
-
-db = SQLAlchemy(app)
-
-class Model(db.Model):
-    
-    id = db.Column(db.Integer, primary_key= True)
-    content = db.Column(db.String(300), nullable = False)
-    complete = db.Column(db.Boolean , default = False)
-    
-    def __repr__(self):
-        return f'<Task {self.id}>'
+db.init_app(app)
   
 
 @app.route('/')
@@ -45,14 +35,14 @@ def edit(id):
     if request.method == "POST":
         new_content = request.form.get("new_content")
         task.content = new_content
-        
+             
         try:
             db.session.commit()
             return redirect('/')
-        
+            
         except:
             return "error occur while editting"
-    
+
 
     return render_template("edit.html", task = task)
 
